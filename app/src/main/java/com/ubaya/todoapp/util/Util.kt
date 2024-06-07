95d626efd62424a618147ae97e65eeb583d52ff5
WIRA <s160421134@student.ubaya.ac.id>
2024-06-07 17:52:23 +0700

    Week 12

--- app/src/main/java/com/ubaya/todoapp/util/Util.kt
+++ app/src/main/java/com/ubaya/todoapp/util/Util.kt
@@ -1,10 +1,23 @@
 package com.ubaya.todoapp.util
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
+import android.graphics.BitmapFactory
+import android.os.Build
+import android.util.Log
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import androidx.room.Room
 import androidx.room.migration.Migration
 import androidx.sqlite.db.SupportSQLiteDatabase
+import androidx.work.Worker
+import androidx.work.WorkerParameters
+import com.ubaya.todoapp.R
 import com.ubaya.todoapp.model.TodoDatabase
+import com.ubaya.todoapp.view.MainActivity
 
 val DB_NAME = "newtododb"
 
@@ -31,3 +44,70 @@ val MIGRATION_2_3 = object : Migration(2, 3) {
         )
     }
 }
+
+class NotificationHelper(val context: Context) {
+    private val CHANNEL_ID = "todo_channel_id"
+    private val NOTIFICATION_ID = 1
+
+    companion object {
+        val REQUEST_NOTIF = 100
+    }
+
+    private fun createNotificationChannel( ) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU){
+            val chanel = NotificationChannel(CHANNEL_ID, CHANNEL_ID, NotificationManager.IMPORTANCE_DEFAULT).apply {
+                description = "Todo channel desc"
+            }
+            val notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE)as NotificationManager
+            notificationManager.createNotificationChannel(chanel)
+        }
+    }
+
+    fun createNotification( title:String, message:String) {
+        createNotificationChannel()
+        val intent = Intent(context, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or
+                    Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+
+        val pendingIntent = PendingIntent.getActivity(context,0, intent, PendingIntent.FLAG_IMMUTABLE)
+        val icon = BitmapFactory.decodeResource(context.resources, R.drawable.todochar)
+
+        val notification = NotificationCompat.Builder(context, CHANNEL_ID)
+            .setSmallIcon(R.drawable.checklist)
+            .setLargeIcon(icon)
+            .setContentTitle(title)
+            .setContentText(message)
+            .setStyle(
+                NotificationCompat.BigPictureStyle()
+                    .bigPicture(icon)
+                    .bigLargeIcon(null)
+            )
+            .setContentIntent(pendingIntent)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .build()
+        try {
+            NotificationManagerCompat.from(context)
+                .notify(NOTIFICATION_ID, notification)
+        } catch (e:SecurityException) {
+            Log.e("error", e.toString())
+        }
+    }
+
+
+
+}
+
+class TodoWorker (context: Context, params:WorkerParameters): Worker(context,params) {
+    override fun doWork(): Result {
+        NotificationHelper(applicationContext).createNotification(
+            inputData.getString("title").toString(),
+            inputData.getString("message").toString())
+        return Result.success()
+
+
+    }
+}
+
+
+
--- app/src/main/java/com/ubaya/todoapp/view/CreateTodoFragment.kt
+++ app/src/main/java/com/ubaya/todoapp/view/CreateTodoFragment.kt
@@ -1,5 +1,7 @@
 package com.ubaya.todoapp.view
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -7,17 +9,52 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.RadioButton
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.Navigation
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.WorkManager
+import androidx.work.workDataOf
 import com.ubaya.todoapp.R
 import com.ubaya.todoapp.databinding.FragmentCreateTodoBinding
 import com.ubaya.todoapp.model.Todo
+import com.ubaya.todoapp.util.NotificationHelper
+import com.ubaya.todoapp.util.NotificationHelper.Companion.REQUEST_NOTIF
+import com.ubaya.todoapp.util.TodoWorker
 
 import com.ubaya.todoapp.viewmodel.DetailTodoViewModel
+import java.util.concurrent.TimeUnit
 
-class CreateTodoFragment : Fragment() {
+class CreateTodoFragment : Fragment(),RadioClickListener,TodoEditClickListener {
     private lateinit var binding: FragmentCreateTodoBinding
     private lateinit var viewModel: DetailTodoViewModel
+
+    override fun onRadioClick(v: View, priority: Int, obj: Todo) {
+        obj.priority = priority
+    }
+    override fun onTodoEditClick(v: View) {
+        val workRequest = OneTimeWorkRequestBuilder<TodoWorker>()
+            .setInitialDelay(30, TimeUnit.SECONDS)
+            .setInputData(
+                workDataOf(
+                    "title" to "Todo created",
+                    "message" to "Stay focus"
+                ))
+            .build()
+        WorkManager.getInstance(requireContext()).enqueue(workRequest)
+
+        val radio = view?.findViewById<RadioButton>(binding.radioGroupPriority.checkedRadioButtonId)
+        var todo = Todo(
+            binding.txtTitle.text.toString(),
+            binding.txtNotes.text.toString(),
+            radio?.tag.toString().toInt()
+        )
+        val list = listOf(binding.todo!!)
+        viewModel.addTodo(list)
+        Toast.makeText(view?.context, "Data added", Toast.LENGTH_LONG).show()
+        Navigation.findNavController(v).popBackStack()
+    }
+
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?
@@ -29,23 +66,35 @@ class CreateTodoFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        if (ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.POST_NOTIFICATIONS)
+            != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(requireActivity(),
+                arrayOf(Manifest.permission.POST_NOTIFICATIONS),NotificationHelper.REQUEST_NOTIF)
+        }
+
         viewModel =
             ViewModelProvider(this).get(DetailTodoViewModel::class.java)
 
-        binding.btnAdd.setOnClickListener {
-            val radio = view.findViewById<RadioButton>(binding.radioGroupPriority.checkedRadioButtonId)
-            var todo = Todo(
-                binding.txtTitle.text.toString(),
-                binding.txtNotes.text.toString(),
-                radio.tag.toString().toInt()
-            )
-            val list = listOf(todo)
-            viewModel.addTodo(list)
-            Toast.makeText(view.context, "Data added", Toast.LENGTH_LONG).show()
-            Navigation.findNavController(it).popBackStack()
-        }
-
+        binding.todo = Todo("","",3)
+        viewModel = ViewModelProvider(this).get(DetailTodoViewModel::class.java)
+        binding.addListener = this
+        binding.radioListener = this
     }
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                            permissions: Array<out String>,
+                                            grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(requestCode ==NotificationHelper.REQUEST_NOTIF) {
+            if(grantResults.isNotEmpty() && grantResults[0] ==
+                PackageManager.PERMISSION_GRANTED) {
+                NotificationHelper(requireContext())
+                    .createNotification("Todo Created",
+                        "A new todo has been created! Stay focus!")
+            }
+        }
+    }
+
 
 
 
